723f4fd58c24b642b36266f081db53768fa1e1af
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2024-08-03 00:35:15 -0300

    add generate company command with cnpj and cpf generators

--- src/Commands/GenerateCompanyCommand.cs
+++ src/Commands/GenerateCompanyCommand.cs
@@ -1,5 +1,8 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Text;
+
+using TerminalDevTools.Generators;
 
 namespace TerminalDevTools.Commands;
 
@@ -21,6 +24,8 @@ public partial class GenerateCommand
         {
             AllowMultipleArgumentsPerToken = true
         }.FromAmong(DataOptions);
+        private readonly Random _random = new();
+
         public GenerateCompanyCommand() : base(name: "company", "Generates a company's data")
         {
             AddOption(_dataOption);
@@ -34,7 +39,23 @@ public partial class GenerateCommand
                 IEnumerable<string> data = context.ParseResult.GetValueForOption(_dataOption) ?? [];
                 if (!data.Any())
                     data = data.Union(DataOptions);
-                // TODO: implement generation here
+                data = data.OrderBy(s => s);
+                StringBuilder output = new();
+                string baseCnpj = _random.BaseCnpj();
+                string cpf = _random.Cpf();
+                foreach (var option in data)
+                {
+                    output.AppendLine(option switch
+                    {
+                        BaseCnpjOptionValueName =>
+                            $"base_cnpj={baseCnpj}",
+                        FullCnpjOptionValueName => $"full_cnpj={_random.FullCnpj(baseCnpj)}",
+                        CpfOptionValueName => $"cpf={cpf}",
+                        CaepfOptionValueName => $"caepf={_random.Caepf(cpf)}",
+                        string any => throw new NotImplementedException($"Option {any} not implemented"),
+                    });
+                }
+                context.Console.WriteLine(output.ToString());
                 context.ExitCode = 0;
             }
             catch (Exception ex)
--- src/Generators/CpfGenerator.cs
+++ src/Generators/CpfGenerator.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace TerminalDevTools.Generators;
 
 public static class CpfGenerator
@@ -21,5 +23,48 @@ public static class CpfGenerator
         result[10] = result[10] % 11 < 2 ? 0 : 11 - (result[10] % 11);
 
         return string.Join("", result.Select(d => $"{d}"));
+    }
+    public static string Caepf(this Random random, string? cpf = null, string seq = "001")
+    {
+        if (cpf is not null && !Regex.IsMatch(input: cpf, pattern: @"^\d{9}\d{0,2}$"))
+            throw new ArgumentException(message: null, paramName: nameof(cpf));
+        if (seq is not null && !Regex.IsMatch(input: seq, pattern: @"^\d{3}$"))
+            throw new ArgumentException(message: null, paramName: nameof(seq));
+        cpf ??= random.Cpf();
+
+        List<int> caepf = (cpf[0..9] + seq)
+            .Select(c => int.Parse($"{c}"))
+            .ToList();
+
+        List<int> checkDigitMultipliers = [6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 9];
+        int checkDigitSum = 0;
+        int checkDigit = 0;
+
+        for (int i = 0; i < caepf.Count; i++)
+            checkDigitSum += caepf[i] * checkDigitMultipliers[i];
+        checkDigit = checkDigitSum % 11;
+        if (checkDigit >= 10)
+            checkDigit = 0;
+        var checkDigits = checkDigit * 10;
+
+        caepf.Add(checkDigit);
+        checkDigitMultipliers.Insert(0, 5);
+
+        checkDigitSum = 0;
+        for (int i = 0; i < caepf.Count; i++)
+            checkDigitSum += caepf[i] * checkDigitMultipliers[i];
+
+        checkDigit = checkDigitSum % 11;
+        if (checkDigit >= 10)
+            checkDigit = 0;
+
+        checkDigits += checkDigit + 12;
+        if (checkDigits >= 100)
+            checkDigits -= 100;
+
+        caepf[12] = checkDigits / 10;
+        caepf.Add(checkDigits % 10);
+
+        return string.Join("", caepf);
     }
 }
